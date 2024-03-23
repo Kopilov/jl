2d4ad23a471beecd95908a5a79343a1a4c3f2398
Kopilov Aleksandr <kopilov.ad@gmail.com>
2024-03-23 22:19:33 +0300

    Do not use Docker:latest

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,2 @@
-docker build .. -f ./Dockerfile-core -t jl-core
-docker build .. -f ./Dockerfile-jupyter -t jl-jupyter
+docker build .. -f ./Dockerfile-core -t jl-core:1.0
+docker build .. -f ./Dockerfile-jupyter -t jl-jupyter:1.0
