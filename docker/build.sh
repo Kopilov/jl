b9384ae55983072f8fbfcdd989d687e44885c99d
Kopilov Aleksandr <kopilov.ad@gmail.com>
2024-03-30 14:26:35 +0300

    statTemplate.xlsx

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,2 @@
-docker build .. -f ./Dockerfile-core -t jl-core:1.0
-docker build .. -f ./Dockerfile-jupyter -t jl-jupyter:1.0
+docker build .. -f ./Dockerfile-core -t jl-core:1.1
+docker build .. -f ./Dockerfile-jupyter -t jl-jupyter:1.1
