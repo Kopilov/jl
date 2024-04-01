d2513601d614669d72f99321f3c04e4032a051fd
Kopilov Aleksandr <kopilov.ad@gmail.com>
2024-04-01 22:24:19 +0300

    Rename docker image tag

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,5 @@
-docker build .. -f ./Dockerfile-core -t jl-core:1.1
-docker build .. -f ./Dockerfile-jupyter -t jl-jupyter:1.1
+docker build .. -f ./Dockerfile-core -t kopilov/jetlend_ru_dashboard:core-1.1
+docker build .. -f ./Dockerfile-jupyter -t kopilov/jetlend_ru_dashboard:jupyter-1.1
+
+#docker push kopilov/jetlend_ru_dashboard:core-1.1
+#docker push kopilov/jetlend_ru_dashboard:jupyter-1.1
